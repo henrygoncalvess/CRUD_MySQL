d7bc41195c232e3aaceb57e5f08f36f5130f38a3
henrygoncalvess <octanebt@gmail.com>
2024-10-20 16:48:16 -0300

    criação de usuário com boas práticas e tratamento de erros

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,25 +2,36 @@ const database = require('../models/db.js')
 
 class UserController{
     async index(req, res){
-        const response = await database.query(`SELECT * FROM ${process.env.TABLE}`)
+        try{
+            const response = await database.query(`SELECT * FROM ${process.env.TABLE}`)
 
-        res.json(response)
+            res.json(response)
+
+        } catch (error){
+            res.sendStatus(400)
+            throw error
+        }
     }
 
     async new(req, res){
-        const { nome, nascimento, sexo, peso } = req.body
+        try{
+            const { nome, nascimento, sexo, peso } = req.body
+            
+            if (!nome || !nascimento || !sexo){
+                throw new Error(`Os campos "nome", "nascimento" e "sexo" são obrigatórios.`);
+            }
 
-        const newUser = await database.query(
-            `INSERT INTO ${process.env.TABLE}
-            (nome, nascimento, sexo, peso)
-            VALUES
-            (?, ?, ?, ?)`,
-            [nome, nascimento, sexo, peso]
-        )
+            const [ addNewUser ] = await database.query(
+                `INSERT INTO ${process.env.TABLE} (nome, nascimento, sexo, peso) VALUES (?, ?, ?, ?)`,
+                [nome, nascimento, sexo, peso]
+            )
 
-        console.log(newUser)
+            res.json(addNewUser)
 
-        res.sendStatus(201)
+        } catch (error){
+            res.sendStatus(400)
+            throw error
+        }
     }
 
     update(req, res){
