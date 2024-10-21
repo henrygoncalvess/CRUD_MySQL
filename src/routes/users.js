8b2c7a3c5559fb6a3e999489a2f9637370ca9b5c
henrygoncalvess <octanebt@gmail.com>
2024-10-21 00:36:31 -0300

    apagando comentários de requisição - users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,3 @@
-// --- 1xx: informação
-
-// --- 2xx: sucesso
-//   --- 200:   OK
-//   --- 201:   CREATED
-//   --- 200:   não tem conteúdo PUT POST DELETE
-
-// --- 3xx: redirection
-
-// --- 4xx: client error
-//   --- 400:   bad request
-//   --- 404:   not found
-
-// --- 5xx: server error
-//   --- 404:   internal server error
-
 const express = require('express')
 const rota = express.Router()
 const UserController = require('../controllers/UserController.js')
